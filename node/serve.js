95a3d12717d1b6b5a67ad5e02fe1def5629f9f70
wenkangluo <wenkangluo@digitalgd.com.cn>
2019-05-27 16:45:28 +0800

    1

---
+++ node/serve.js
@@ -0,0 +1,19 @@
+var express=require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('sentTH',msg=>{
+    io.sockets.emit('getTH',msg);
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
